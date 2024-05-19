636e9d56fd95c60670bd3c6b8e2c6fcfbcd6aec3
Sasha Chun <cunsasa878@gmail.com>
2024-05-19 13:59:58 +0300

    END DEV CREATE

--- saveonrent/src/assets/creactor/Creates.jsx
+++ saveonrent/src/assets/creactor/Creates.jsx
@@ -18,17 +18,54 @@ export default function Creates() {
   const [submittedAboutMYself, setSubmittedAboutMYself] = useState();
   const [activeIndex, setActiveIndex] = useState(false);
   const [stateChange, setStateChange] = useState({ state1: false });
+  const [submittedArea , setsubmittedStateArea] = useState("Area");
+  const [stateArea , setStateArea] = useState();
+  const [stateCitu , setStateCitu] = useState();
+  const [submittedCitu , setsubmittedCitu] = useState("luboml");
+  const [stateBudgetMin , setBudgetMin ] = useState();
+  const [submittedBudgetMin , setSubmitteBudgetMin ] = useState("0000");
+  const [stateBudgetMax , setBudgetMax] = useState();
+  const [submittedBudgetMax , setSubmittedBudgetMax] = useState("0000" );
+  const [stateRentalTerm ,setRentalTerm] =useState();
+  const [submittedRentalTerm ,setSubmittedRentalTerm] =useState ();
+  const [submittedPhone ,setSubmittedPhone] =useState();
+  const [statePhone ,setPhone] =useState();
+  const [stateGmail ,setGmail] =useState();
+  const [submittedGmail ,setSubmittedGmail] =useState();
 
+  function handlePhone(event){
+      setPhone(event.target.value)
+  }
+  
+  function handleGmail(event){
+    setGmail(event.target.value)
+  }
+  
+  function handleBudgetMax(event){
+    setBudgetMax(event.target.value)
+  }
+  function handleBudgetMin(event){
+    setBudgetMin(event.target.value)
+  }
+
+  function handleArea(event){
+    setStateArea(event.target.value)
+  }
+  function handleCity(event){
+    setStateCitu(event.target.value)
+  }
   function handleClick(event) {
     const { name, checked } = event.target;
     setStateChange((prevState) => ({ ...prevState, [name]: checked }));
   }
-  function handleLine(index) {
-    setActiveIndex(index);
+function handleLine(index) {
+ setActiveIndex(index);
   }
+  
   function handleName(event) {
     setNameState(event.target.value);
   }
+
 
   function handleAboutMYself(event) {
     setAboutMYself(event.target.value);
@@ -47,8 +84,13 @@ export default function Creates() {
     setSubmittedJob(stateJob);
     setSubmittedAge(stateAge);
     setSubmittedAboutMYself(stateAboutMYself);
+    setsubmittedStateArea(stateArea);
+    setsubmittedCitu(stateCitu);
+    setSubmittedBudgetMax(stateBudgetMax);
+    setSubmitteBudgetMin(stateBudgetMin);
+    setSubmittedPhone(statePhone);
+    setSubmittedGmail(stateGmail);
   }
-
   return (
     <section className="sectionCreate">
       <div className="CreateBLOX">
@@ -81,10 +123,10 @@ export default function Creates() {
               onChange={handleAboutMYself}
               defaultValue={"Additional Info"}
             />
-            <BigL def={"Desired areas (max 3)"} />
-            <BigL def={"City"} />
-            <LoveL def1={"Budget Min"} def2={"Budget Max"} />
-            <MidleL def1={"Bedrooms"} def2={"Bathrooms"} />
+            <BigL def={"Desired areas (max 3)"} change={handleArea} />
+            <BigL def={"City"} change={handleCity} />
+            <LoveL def1={"Budget Min"} change1={handleBudgetMax} change2={handleBudgetMin} def2={"Budget Max"} />
+            <MidleL def1={"Bedrooms"}  def2={"Bathrooms"} />
             <div class="parent">
               <SheckFurnished
               inputNames={"orangs"}
@@ -125,12 +167,12 @@ export default function Creates() {
   inputNames={"blus"}
   chil={"Gym"}
   checked={stateChange.state6}
-  name={"state6"}
+  name={"state6"}s
   onChange={handleClick}
 />
             </div>
-            <BigL />
-            <BigL />
+            <BigL change={handlePhone} def={"phone"}/>
+            <BigL change={handleGmail} def={"gmail"}/>
             <button className="SubmitBTN" onClick={handleSubmit}>
               Submit
             </button>
@@ -159,8 +201,22 @@ export default function Creates() {
               for rent in los angeles
             </div>
             <div className="aboutmy">about myself:</div>
-            <div className="aboytmytext"></div>
-            {submittedAboutMYself}
+            <div className="aboytmytext"> {submittedAboutMYself} </div>
+            <div className="Area-budget">
+            <div className="Area">
+            <p className="areaName">Area :</p>
+            <p className="areaText">{submittedArea}</p>
+            </div> 
+            <div className="budget">
+            <p className="areaName">Budget :</p>
+            <p className="areaText">{submittedBudgetMax}+{submittedBudgetMin}</p>
+            </div>
+            </div> 
+
+            <div className="Citu ">
+            <p className="CituName">Citu :</p>
+            <p className="CituText">{submittedCitu}</p>
+            </div> 
             <div className="ChangeItem">
               {stateChange.state1 ? (
                 <Change
@@ -195,6 +251,19 @@ export default function Creates() {
               {stateChange.state6 ? (
                 <Change colors={{ backgroundColor: "#006DBA" }} name={"gym"} />
               ) : undefined}
+            </div>
+            <div className="phones_mail_madeWith">
+                <div className="madeWith">
+                <div className="madeLine"></div>
+                  <div className="madeText">
+                      <div className="With">MADE WITH</div>
+                      <div className="save">SAVEONRENT.ORG</div>
+                  </div>
+                </div>
+                <div className="mailPhone">
+                  <div className="phone">email:{submittedPhone}</div>
+                  <div className="mail">phone:{submittedGmail}</div>
+                </div>
             </div>
           </div>
         </div>
--- saveonrent/src/assets/creactor/inputFolder/LoveI.jsx
+++ saveonrent/src/assets/creactor/inputFolder/LoveI.jsx
@@ -1,7 +1,7 @@
-export default function LoveL({val , change, def1 , def2 }){
+export default function LoveL({val , change1, change2, def1 , def2 }){
     return<div className="setLOve">
-<input type="text" value={val} onChange={change} defaultValue={def1} className = "LoveL" />
-<input type="text" value={val} onChange={change} defaultValue={def2} className = "LoveL" />
+<input type="text" value={val} onChange={change1} defaultValue={def1} className = "LoveL" />
+<input type="text" value={val} onChange={change2} defaultValue={def2} className = "LoveL" />
 <select  className = "LoveL" name="unittype">
   <option value="1"> Miner </option>
   <option value="2"> Puffer </option>
