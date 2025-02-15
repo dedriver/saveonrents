075cac562d0772433a313c794a049558eb2764ed
Саша Чунь <chunsasha273@gmail.com>
2025-02-15 02:01:30 +0200

    Update App.jsx

--- saveonrent/src/App.jsx
+++ saveonrent/src/App.jsx
@@ -1,24 +1,59 @@
-import './App.css'
-import './reset.css'
-import Heder from './heder/Heder.jsx'
-import Flagman from './flagman/Flagman.jsx'
-import Creates from './assets/creactor/Creates.jsx'
-import Foter from './fotter/Foter.jsx'
-import './media.css'
-function App() {
-  return (
-    <>
-      <Heder/>
-      <main>
-        <Flagman/>
-       <section> <h3 className='CreateMainSlogan'>Create infographics<br/>
-for free</h3></section>
-         <Creates/> 
-      </main>
-          <Foter />
-     <p>das</p>
-    </>
-  )
-}
+import MessageBox from "../allChats/MessageBox.jsx";
+import { useEffect, useMemo, useState } from "react";
+import { useSession } from "next-auth/react";
+import { pusherClient } from "../../../libe/pucher.js";
+import {find} from "loadsh/collection.js";
 
-export default App {*/test/*}
+export default function SideBarMessage({ conversations }) {
+    const { data: session } = useSession(); // Отримуємо поточного користувача
+
+    const [conversationsList, setConversationsList] = useState(conversations);
+
+    // Створюємо ключ для підписки
+    const pusherKey = useMemo(() => {
+        return session?.user?.email; // Переконаємося, що дані існують
+    }, [session?.user?.email]);
+
+    useEffect(() => {
+        if (!pusherKey) return; // Якщо користувача немає, не підписуємося
+
+        pusherClient.subscribe(pusherKey);
+
+        const newHandler = (conversation) => {
+            setConversationsList((current) => {
+                if (find(current, { id: conversation.id })) {
+                    return current; // Якщо вже є — не додаємо
+                }
+                return [conversation, ...current];
+            });
+        };
+
+        pusherClient.bind("conversation:new", newHandler);
+
+        return () => {
+            pusherClient.unsubscribe(pusherKey);
+            pusherClient.unbind("conversation:new", newHandler);
+        };
+    }, [pusherKey]);
+
+    return (
+        <div className="flex h-[60vh] flex-col">
+            <p className="text-2xl p-[15px]">Messages</p>
+            <div>
+                {conversationsList.length > 0 ? (
+                    conversationsList.map((event, index) => (
+                        <MessageBox
+                            key={index}
+                            find={false}
+                            lastMessage={"hallow"}
+                            name={event.users[0].name}
+                            data={event}
+                        />
+                    ))
+                ) : (
+                    <p className="flex items-center justify-center h-[50vh]">No messages</p>
+                )}
+            </div>
+        </div>
+    );
+}
